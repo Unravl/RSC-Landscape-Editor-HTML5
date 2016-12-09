354696a006d5f5067ec1ccde300e818a2a401518
xEnt22 <xent22@gmail.com>
2016-12-09 10:39:33 +1030

    Added basic vertical/horizontal walls + clean up
    needs proper wall textures added for more types other than the normal.

--- src/Editor.js
+++ src/Editor.js
@@ -26,6 +26,7 @@ function openFile(event) {
     jsZip.loadAsync(file).then(function (zip) {
         var x=50, y=47;
         var offsets = [[-1, -1],[0, -1],[1, -1], [-1, 0],[0, 0],[1, 0], [-1, 1],[0, 1],[1, 1]];
+        // var offsets = [[0, 0]];
         for(var i=0; i < offsets.length; i++) {
             var name = "h0x" + (x + offsets[i][0]) + "y" + (y + offsets[i][1]);
             console.error("reading " + name);
@@ -86,7 +87,7 @@ function loadSectors() {
         var geometry = new THREE.PlaneGeometry((TILE_MESH_SIZE * TILE_COUNT), (TILE_MESH_SIZE * TILE_COUNT), TILE_COUNT, TILE_COUNT);
         for (var x = 0; x < TILE_COUNT; x++) {
             for (var y = 0; y < TILE_COUNT; y++) {
-                var tile = new Tile(sectorIndex + 1, sectX, sectY, x, y + (s * 48), view.getUint8(idx), view.getUint8(idx + 1), view.getUint8(idx + 2), view.getUint8(idx + 3), view.getUint8(idx + 4), view.getUint8(idx + 5), view.getUint32(idx + 6), s);
+                var tile = new Tile(sectorIndex + 1, sectX, sectY, x, y, view.getUint8(idx), view.getUint8(idx + 1), view.getUint8(idx + 2), view.getUint8(idx + 3), view.getUint8(idx + 4), view.getUint8(idx + 5), view.getUint32(idx + 6), s);
 
                 Tiles[((x * 48) + y) * (sectorIndex + 1)] = tile;
                 idx += 10;
@@ -94,10 +95,6 @@ function loadSectors() {
         }
 
 
-        /**
-         *
-         * ELEVATION MESSIN AROUND. CANT SEEM TO GET THE CORRECT VERTICES OF EACH TILE, TO GO HIGHER/LOWER.
-         */
         // set elevation
         for (var xx = 0; xx < 48; xx++) {
             for (var yy = 0; yy < 48; yy++) {
@@ -153,65 +150,44 @@ function loadSectors() {
                 geometry.faces[j + 1].materialIndex = 6;
             }
 
-            /**
-             *
-             *
-             *
-             * REWRITE the below things, possibly in a new format like above, for fps.
-             */
-            if (til.verticalWall > 0) {
-
-              /*  var overlayGeom = new THREE.PlaneGeometry(1, 1);
-                var wall = new THREE.Mesh(overlayGeom, materials[4]);
-                wall.rotation.x = Math.PI / 2;
-                wall.position.x = (til.x * TILE_MESH_SIZE);
-                wall.position.y = til.y * TILE_MESH_SIZE + (TILE_MESH_SIZE / 2);
-                wall.position.z = til.cube.position.z + 0.3 + ((til.groundElevation/ 50) / 5);
-                console.log(wall.position.z);
-
-                scene.add(wall);*/
-            }
-            if (til.horizontalWall > 0) {
-
-                /*var overlayGeom = new THREE.PlaneGeometry(1, 1);
-                 var wall = new THREE.Mesh(overlayGeom, wallVertMat);
-                 wall.rotation.y = Math.PI / 2;
-                 wall.position.x = (tile.x * TILE_MESH_SIZE) + (TILE_MESH_SIZE / 2);
-                 wall.position.y = tile.y * TILE_MESH_SIZE;
-                 wall.position.z = tile.cube.position.z + 0.3 + ((tile.groundElevation/ 50) / 5);
-                 console.log(wall.position.z);
-
-                 scene.add(wall);
-                 */
-            }
-
-
-            if (til.diagonalWall == 1) {
-                /*
-                 var overlayTexture = new THREE.ImageUtils.loadTexture( 'img/3222.png' );
-                 var overlayMat = new THREE.MeshBasicMaterial( { map: overlayTexture, side:THREE.DoubleSide } );
-                 var overlayGeom = new THREE.PlaneGeometry(1, 1);
-                 var wall = new THREE.Mesh(overlayGeom, wallMat);
-                 wall.rotation.y = Math.PI / 2;
-                 //  wall.rotation.x = Math.PI / 8
-                 //wall.rotation.x = Math.PI / 4; // corner
-                 wall.position.x = (tile.x * TILE_MESH_SIZE) + (TILE_MESH_SIZE / 2);
-                 wall.position.y = tile.y * TILE_MESH_SIZE;
-                 wall.position.z = tile.cube.position.z + 1.5;
-                 console.log(wall.position.z);
-                 */
-                //  scene.add(wall);
-            }
-
-
         }
 
+        /**
+         * Wall loading, clean this up make code smaller etc
+         *
+         */
+        var tempGeom = new THREE.PlaneGeometry(0, 0);
+        var tempMesh = new THREE.Mesh(tempGeom, new THREE.MeshFaceMaterial(materials));
         l = geometry.faces.length / 2;
         for (var i = 0; i < l; i++) {
             var j = 2 * i;
             var til = Tiles[i * (sectorIndex + 1)];
             if (til == null)
                 continue;
+
+            if (til.horizontalWall > 0) {
+
+
+                var overlayGeom = new THREE.PlaneGeometry(1, 1);
+                overlayGeom.faces[0].materialIndex = 3;
+                overlayGeom.faces[1].materialIndex = 3;
+                var wall = new THREE.Mesh(overlayGeom, new THREE.MeshFaceMaterial(materials));
+                wall.rotation.y = Math.PI / 2;
+                wall.position.x = til.x - (TILE_COUNT / 2);
+                wall.position.y = til.y - (TILE_COUNT / 2) + 0.5;
+                var base = 0;
+                var multi = 0.04;
+                if(til.groundElevation < 128) {
+                    base -= multi * (128 - til.groundElevation);
+                } else {
+                    base += multi * Math.abs((128 - til.groundElevation));
+                }
+                wall.position.z = base + 0.5;
+
+                wall.updateMatrix();
+                tempGeom.merge(wall.geometry, wall.matrix);
+            }
+
             if (til.verticalWall > 0) {
 
                 var overlayGeom = new THREE.PlaneGeometry(1, 1);
@@ -219,47 +195,42 @@ function loadSectors() {
                 overlayGeom.faces[1].materialIndex = 2;
                 var wall = new THREE.Mesh(overlayGeom, new THREE.MeshFaceMaterial(materials));
                 wall.rotation.x = Math.PI / 2;
-                wall.position.x = (til.x );
-                wall.position.y =( til.y  );
+                wall.position.x = til.x - (TILE_COUNT / 2) + 0.5;
+                wall.position.y = til.y - (TILE_COUNT / 2)
 
-              //  wall.position.set(til.x, til.y, 10);
-              //  wall.position.z = ((til.groundElevation/ 20) + 5);
+                var base = 0;
+                var multi = 0.04;
+                if(til.groundElevation < 128) {
+                    base -= multi * (128 - til.groundElevation);
+                } else {
+                    base += multi * Math.abs((128 - til.groundElevation));
+                }
+                wall.position.z = base + 0.5;
+
                 wall.updateMatrix();
-                geometry.merge(wall.geometry, wall.matrix);
-               // scene.add(wall);
+                tempGeom.merge(wall.geometry, wall.matrix);
             }
         }
 
 
-        // mesh
+        tempMesh.rotation.z = -Math.PI / 2;
+        tempMesh.updateMatrix();
+        geometry.merge(tempMesh.geometry, tempMesh.matrix);
+
         var tmesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
         tmesh.position.set(-(TILE_MESH_SIZE * TILE_COUNT / 2) - 48 * sectX, -(TILE_MESH_SIZE * TILE_COUNT / 2) - 48  * sectY, 0);
-       tmesh.rotation.z = -Math.PI / 2
-       // scene.add(tmesh);
-      // tmesh.rotateZ( Math.PI);
+        tmesh.rotation.z = -Math.PI / 2;
+
         tmesh.updateMatrix();
         masterGeometry.merge(tmesh.geometry, tmesh.matrix);
+
+
         sectX++;
 
     }
 
     mesh = new THREE.Mesh(masterGeometry, new THREE.MeshFaceMaterial(materials));
-    // mesh.position.set(-(TILE_MESH_SIZE*TILE_COUNT / 2) - 48, -(TILE_MESH_SIZE*TILE_COUNT / 2) - 48, 0);
-   // mesh.position.set(-(TILE_MESH_SIZE * TILE_COUNT / 2) - 48 * sectX, -(TILE_MESH_SIZE * TILE_COUNT / 2) - 48  * sectY, 0);
-
     scene.add(mesh);
-
-
-      /* for(var x=0; x < TILE_COUNT; x++) {
-            Tiles[x] = new Array(TILE_COUNT);
-            for(var y=0; y < TILE_COUNT; y++) {
-              //  var tile = Tiles[x][y];
-                var tile = Tiles[(x*48)+y];
-                addTile(tile);
-            }
-        }
-
-    }*/
 
     animate();
 }
@@ -268,16 +239,8 @@ function setup() {
 
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-
-
-    //camera.position.set(-40, -40, 80);
-   // camera.position.y = -22.635656355403956;
-   // camera.position.x = -21.190077055448253;
- //   camera.position.set( -11.148991658901329, -28.942319855662255, 39.94132960553199 );
-  //  camera.rotation.set( 0.7650157386463888, -0.00138910089589484717, 0.0013335966942509471 );
     var light = new THREE.AmbientLight(  0x404040 ); // soft white light
     scene.add( light );
-    //camera.up = new THREE.Vector3(0,0,1);
     raycaster = new THREE.Raycaster();
     mouse = new THREE.Vector2();
     renderer = new THREE.WebGLRenderer();
@@ -289,7 +252,7 @@ function setup() {
 
     camera.updateProjectionMatrix();
 
- //   controls.update();
+    //   controls.update();
     //document.addEventListener( 'mousedown', onDocumentMouseDown, false );
     //  document.addEventListener( 'keydown', onDocumentKeyDown, false );
     //  document.addEventListener( 'keyup', onDocumentKeyUp, false );
@@ -304,14 +267,14 @@ function animate() {
     renderer.render(scene, camera);
 
     if(controls != null)
-      controls.update();
+        controls.update();
 }
 
 function html() {
     stats = new Stats();
     stats.showPanel( 0 );
     document.body.appendChild( stats.dom );
-   var top = document.createElement('div');
+    var top = document.createElement('div');
     top.innerHTML = '<input type="file" onchange="openFile(event)" > <button id="rendButton">Render</button><br><br>';
     document.body.appendChild(top);
     container = document.createElement('div');
@@ -321,7 +284,7 @@ function html() {
     info.style.top = '10px';
     info.style.width = '100%';
     info.style.textAlign = 'center';
-   // info.innerHTML = '<input type="file" onchange="openFile(event)" ><br><br>';
+    // info.innerHTML = '<input type="file" onchange="openFile(event)" ><br><br>';
 
     container.appendChild(info);
     container.appendChild(renderer.domElement);
@@ -331,24 +294,16 @@ function html() {
     var button = document.getElementById("rendButton");
     button.addEventListener("click",function(e){
         loadSectors();
-         controls = new THREE.OrbitControls( camera, renderer.domElement );
-         controls.enableDamping = true;
-         controls.dampingFactor = 0.25;
-      //  controls.target.set(-23.248868591147353, -29.39917688607892, 0);
-     //   camera.rotation.set(0, 0, 0);
+        controls = new THREE.OrbitControls( camera, renderer.domElement );
+        controls.enableDamping = true;
+        controls.dampingFactor = 0.25;
         controls.zoomSpeed = 0.5;
-       // controls.target.set(-20, -20, 200);
-      //  controls.target.set(0, 0, 0);
-     //   camera.rotation.set(0, 0, 0);
         camera.position.set(0, 0, 200);
         controls.enabled = false;
-
-       // controls.position.set(-40, -40, 80);
         controls.enabled = true;
         controls.enablePan = true;
         controls.enableZoom = true;
         controls.update();
-       // camera.lookAt(new THREE.Vector3(0,10,0));
 
 
     },false);
@@ -410,12 +365,12 @@ function onDocumentMouseDown( event ) {
 
 function onDocumentKeyDown( event ) {
     /*switch( event.keyCode ) {
-        case 16: isShiftDown = true; break;
-    }*/
+     case 16: isShiftDown = true; break;
+     }*/
 }
 
 function onDocumentKeyUp( event ) {
-   /* switch( event.keyCode ) {
-        case 16: isShiftDown = false; break;
-    }*/
+    /* switch( event.keyCode ) {
+     case 16: isShiftDown = false; break;
+     }*/
 }
